e1b5d63e0562607b58c52d3dd3a57b3a63925f95
MrTangle <alexibanez86@gmail.com>
2023-02-20 17:40:45 +0100

    Funciones clásicas

--- p1.py
+++ p1.py
@@ -1,8 +1,15 @@
 def sumaTodos(limitTo):
     resultado = 0
-    for i in range(0, limitTo):
-        resultado +=1
+    for i in range(0, limitTo+1):
+        resultado += i
         
     return resultado
 
+def sumaTodosLosCuadrados(limitTo):
+    resultado = 0
+    for i in range(limitTo+1):
+        resultado += i*i
+    return resultado
+
 print(sumaTodos(100))
+print(sumaTodosLosCuadrados(3))
