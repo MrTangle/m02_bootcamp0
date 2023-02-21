da47d4f3b3a49f5c6d06349ca610680bf9da0846
MrTangle <alexibanez86@gmail.com>
2023-02-21 12:15:38 +0100

    Lambdas

--- funciones1nivel.py
+++ funciones1nivel.py
@@ -4,6 +4,9 @@ def normal(x):
 def cuadrado(y):
     return y * y
 
+def cubo(x):
+    return x**3
+
 def sumaTodos(limitTo, f):
     resultado = 0
     for i in range(limitTo+1):
@@ -11,5 +14,8 @@ def sumaTodos(limitTo, f):
         
     return resultado
 
-print(sumaTodos(100, normal))
-print(sumaTodos(3, cuadrado))
+if __name__ == "__main__":
+    print(sumaTodos(100, normal))
+    print(sumaTodos(3, cuadrado))
+else:
+    print(__name__)
---
+++ funcionesLambda.py
@@ -0,0 +1,7 @@
+from funciones1nivel import sumaTodos
+
+doble = lambda x: x*2
+triple = lambda x: x**3
+
+print(sumaTodos(3, doble))
+print(sumaTodos(3, triple))
